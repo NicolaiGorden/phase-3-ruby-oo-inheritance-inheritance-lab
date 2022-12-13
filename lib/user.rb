3ef4a9f41b738b0b851cc4cd1ec94cd0a23400df
Nicolai <Nicolaigorden@gmail.com>
2022-12-12 19:07:40 -0500

    commit tree 2007566e4f1bdcf295406672be1213ab6cd252d8

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+    attr_accessor :first_name, :last_name
+    def first_name=(first)
+        @first_name = first
+    end
+
+    def last_name=(last)
+        @last_name = last
+    end
+end
